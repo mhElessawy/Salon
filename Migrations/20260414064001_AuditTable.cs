943d1d15d730c2df0f5d2225ac79d658bf769e43
elessawy <melessawyh@gmail.com>
2026-04-14 10:00:42 +0300

    14-4-2026V2

---
+++ Migrations/20260414064001_AuditTable.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Salon.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BasicSalary",
+                table: "Employees",
+                type: "decimal(18,3)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentNavId",
+                table: "Employees",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Module = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EntityId = table.Column<int>(type: "int", nullable: true),
+                    IpAddress = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DepartmentNavId",
+                table: "Employees",
+                column: "DepartmentNavId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentNavId",
+                table: "Employees",
+                column: "DepartmentNavId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentNavId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_DepartmentNavId",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentNavId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BasicSalary",
+                table: "Employees",
+                type: "decimal(18,3",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,3)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+    }
+}
