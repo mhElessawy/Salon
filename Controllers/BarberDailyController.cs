bbced324a0778efdf0e1877a0ae49650cc4ca744
elessawy <melessawyh@gmail.com>
2026-07-08 00:25:48 +0300

    8-7-2026 v1

--- Controllers/BarberDailyController.cs
+++ Controllers/BarberDailyController.cs
@@ -244,7 +244,7 @@ namespace Salon.Controllers
                     InvoiceCount = empSales.Count,
                     TotalSales = empTotal,
                     Discount = empDiscount,
-                    InstantCollection = empCash + empKNet,
+                    NetCollected = empTotal - (empDebtToEmployee + empDebtToOwner + empAdv + empServiceCommission),
                     Cash = empCash,
                     KNet = empKNet,
                     EmployeeDebt = empDebtToEmployee,
