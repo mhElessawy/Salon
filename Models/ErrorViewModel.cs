1c55747f49de074e116be57e5f8da1f5970c859f
elessawy <melessawyh@gmail.com>
2026-07-26 15:20:47 +0300

    26-7-2026 v1

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -5,5 +5,11 @@ namespace Salon.Models
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public string? ExceptionType { get; set; }
+        public string? ExceptionMessage { get; set; }
+        public string? StackTrace { get; set; }
+
+        public bool ShowExceptionDetails => !string.IsNullOrEmpty(ExceptionMessage);
     }
 }
